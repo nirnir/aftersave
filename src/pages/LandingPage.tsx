ce1be65e04354e490706cfe1c34a76595c816a0a
nirnir <nir.erlich@gmail.com>
2026-02-18 10:42:57 +0000

    Add LandingPage component with welcome message

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const LandingPage: React.FC = () => {
+    return (
+          <div className="landing-page">
+                <h1>Welcome to AfterSave</h1>
+                <p>Never overpay again.</p>
+          </div>
+        );
+};
